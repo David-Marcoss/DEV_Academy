6b691dec13073d5e3352038e67300251db82d543
Pedro Azevedo <pedroazevedo141@gmail.com>
2022-08-01 18:18:07 -0300

    [FIXED] os diretorios dos templates para a remocao de bugs relacionado aos statics.

--- paginas/views.py
+++ paginas/views.py
@@ -3,8 +3,8 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class contatoview(TemplateView):
-    template_name = "contact.html"
+    template_name = "paginas/contact.html"
 
 
 class homeview(TemplateView):
-    template_name = "home.html"
+    template_name = "paginas/home.html"
