1930da86e91b6afbade2166b178d2aff217c9946
David Marcos <ddragondavi@gmail.com>
2022-08-05 12:15:01 -0300

    add arquivos

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import cursosview,detalhes_cursoview
+from .views import cursosview,detalhes_cursoview,CadastroCursoview
 
 
 urlpatterns = [
     path('cursos/',cursosview.as_view(),name='cursos'),
     path('cursos/detalhes/<str:slug>/',detalhes_cursoview,name='detalhes-curso'),
+    path('cursos/cadastrar/',CadastroCursoview.as_view(),name='cadastrar-curso'),
 
 ]
