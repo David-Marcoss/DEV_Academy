b89080095cc7435d865972b65ce87fffd9036213
Pedro Azevedo <pedroazevedo141@gmail.com>
2022-08-16 13:17:23 -0300

    [ADDED] os cursos na pagina home.

--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from cursos.models import modelcursos
 
 # Create your views here.
 
@@ -8,3 +9,9 @@ class contatoview(TemplateView):
 
 class homeview(TemplateView):
     template_name = "paginas/home.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        todos_cursos = modelcursos.objects.all()      
+        context['top_cursos'] = todos_cursos[:6]
+        return context
