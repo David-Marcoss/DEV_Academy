a585ed3007981a7c7143a9b17e94c639a8827f3d
Pedro Azevedo <pedroazevedo141@gmail.com>
2022-08-25 22:57:47 -0300

    [ADDED] imagens na pagina home.

--- paginas/views.py
+++ paginas/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from cursos.models import modelcursos
+from accounts.models import User
 
 # Create your views here.
 
@@ -12,6 +13,8 @@ class homeview(TemplateView):
     
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        todos_cursos = modelcursos.objects.all()      
-        context['top_cursos'] = todos_cursos[:6]
+        allProf = User.objects.all().filter(is_Teacher=True).values()
+        context['Prof_01'] = allProf[:1][0] if allProf[:1] else None
+        context['Prof_02'] = allProf[1:2][0] if allProf[1:2] else None
+        context['Prof_03'] = allProf[2:3][0] if allProf[2:3] else None
         return context
